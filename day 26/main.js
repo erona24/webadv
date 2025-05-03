2bb11509109515854856dd100fc5904422bbc902
erona24 <eronakrasniqi468@gmail.com>
2025-05-03 12:46:51 +0000

    javascript

---
+++ day 26/main.js
@@ -0,0 +1,46 @@
+document.write("Hello World!")
+
+var x = 19;
+var name = "Erona"
+
+document.write(x);
+console.log(name);
+
+var s = 49 + 39;
+console.log(s)
+
+var y = 49;
+var z = 39;
+var yz = y - z;
+
+console.log(yz)
+
+var n = 89;
+n++;
+console.log(n);
+
+var o = 99;
+o--;
+console.log(o);
+
+var a = 13;
+var r = a ** 2;
+console.log(r);
+
+var f = 15;
+ f+=5 // 15 + 5 = 20
+console.log(f);
+
+var e = 35;
+e*=5;
+console.log(e)
+
+var num1 = 5;
+var num2 = 10;
+var num3 = 5;
+console.log(num1 != num2);
+console.log(num1 == num3);
+console.log(num1 > num3);
+console.log(num1 >= num3);
+console.log(num1 <= num2);
+console.log(num1 <= num2);
